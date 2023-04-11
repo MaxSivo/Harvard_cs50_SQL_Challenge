d7b51097046f6b59a373d3c435e113cca291a92b
Max Sivolella <101618264+MaxSivo@users.noreply.github.com>
2023-04-10 19:08:52 -0700

    Update learnSQLsolutions.sql

--- learnSQLsolutions.sql
+++ learnSQLsolutions.sql
@@ -390,5 +390,22 @@ GROUP BY pr.province_name
 having 
 	count(case when gender = 'M' then 1 end) > 
   count(case when gender = 'F' then 1 end)
+  
+-- We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
+-- First_name contains an 'r' after the first two letters.
+-- Identifies their gender as 'F'
+-- Born in February, May, or December
+-- Their weight would be between 60kg and 80kg
+-- Their patient_id is an odd number
+-- They are from the city 'Kingston'
+
+SELECT * 
+FROM patients
+WHERE first_name like '__r%'
+and gender = 'F'
+and weight between 60 and 80
+and patient_id % 2 != 0 
+and city = 'Kingston'
+and month(birth_date) in (2, 5, 12)
 
 
